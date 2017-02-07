4aba8f889fa80153c1e320ee8b2781e96647c3cb
Justyna Foster <justynawfoster@gmail.com>
2017-02-07 12:17:43 -0700

    set up server.js

--- server.js
+++ server.js
@@ -2,14 +2,24 @@
 
 const express = require('express');
 const app = express();
+const messages = require('./routes/classifieds');
 const bodyParser = require('body-parser');
 
-const messages = require('./routes/classifieds');
-
+app.use(bodyParser.urlencoded({
+  extended:false
+}));
 app.use(bodyParser.json());
+app.use(express.static('./public'));
 app.use('/classifieds', messages);
 
+app.use('/jquery', express.static('node_modules/jquery/dist'));
+app.use('/bootstrap', express.static('node_modules/bootstrap/dist/js'));
+app.use('/angular', express.static('node_modules/angular'));
+app.use('/ui-bootsratp', express.static('node_modules/ui-bootsrtap'));
+app.use('/angular-ui-router', express.static('node_modules/angular-uirouter/release'));
+
 const port = process.env.PORT || 3000;
+
 
 app.listen(port, () => {
     console.log('Listening on port', port);
